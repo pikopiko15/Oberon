ec8adf892856be357c769eab875d8417561bd9b2
Kemal Maralov <Kemal_Maralov@epam.com>
2024-02-20 17:25:12 +0600

    Add project files.

---
+++ OberonPetProject/Program.cs
@@ -0,0 +1,35 @@
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.CreateUmbracoBuilder()
+            .AddBackOffice()
+            .AddWebsite()
+            .AddDeliveryApi()
+            .AddComposers()
+            .Build();
+
+        WebApplication app = builder.Build();
+
+        await app.BootUmbracoAsync();
+
+
+        app.UseUmbraco()
+            .WithMiddleware(u =>
+            {
+                u.UseBackOffice();
+                u.UseWebsite();
+            })
+            .WithEndpoints(u =>
+            {
+                u.UseInstallerEndpoints();
+                u.UseBackOfficeEndpoints();
+                u.UseWebsiteEndpoints();
+            });
+
+        await app.RunAsync();
+    }
+}
